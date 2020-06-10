b359f6a146c2569dde6ebb77e4a969e9cd822d8f
Kimuhjin <ajin950714@naver.com>
2020-06-11 00:22:45 +0900

    Updates

--- precache-manifest.9d71781e5fea818fc7d41dbd134ed597.js
+++ precache-manifest.9d71781e5fea818fc7d41dbd134ed597.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9259ba926c1ef593363c09992d8cdd0e",
+    "revision": "9599a0918b455bcc5864ac50e0294c1c",
     "url": "/Web-Clone/index.html"
   },
   {
-    "revision": "35ac6fb0353cecef116f",
+    "revision": "dbb1c3adea4763caff5d",
     "url": "/Web-Clone/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Web-Clone/static/js/2.a60a710d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "35ac6fb0353cecef116f",
-    "url": "/Web-Clone/static/js/main.5052e34e.chunk.js"
+    "revision": "dbb1c3adea4763caff5d",
+    "url": "/Web-Clone/static/js/main.a2f81e77.chunk.js"
   },
   {
     "revision": "d94f29355c04ecf096ac",
